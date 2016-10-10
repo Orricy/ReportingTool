32bc624f4b4efb4905bee1931922a7414d58be47
Oricy <d.duvernois@student.isartdigital.com>
2016-10-10 16:01:43 +0200

    put script in footer

--- templates/_header.php
+++ templates/_header.php
@@ -5,9 +5,6 @@
     <title>PBP</title>
     <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <script type="text/javascript" src="lib/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="lib/bootstrap/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="model/functions.fn.js"></script>
 </head>
 <body onresize="getSize()">
 	<noscript>
