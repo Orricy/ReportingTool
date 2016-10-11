3ed88b669cd0e6ba0b7b4cb7819e72cd02731312
Stikwood <lalbatm@gmail.com>
2016-10-11 18:09:16 +0200

    commit pour pull

--- form2.php
+++ form2.php
@@ -11,39 +11,39 @@ include('templates/_header.php');
 
         <form>
 
-            <label>Pitch du projet :</label><br>
-            <input type="text" name="pitch" class="form-control" placeholder="Écrivez le pitch du projet"><br><br>
+            <label for="pitch">Pitch du projet :</label><br>
+            <input type="text" id="pitch" class="form-control" placeholder="Écrivez le pitch du projet"><br>
 
-            <label>Brief du projet :</label><br>
-            <input type="text" name="brief" class="form-control" placeholder="Écrivez le brief du projet"><br><br>
+            <label for="brief">Brief du projet :</label><br>
+            <input type="text" id="brief" class="form-control" placeholder="Écrivez le brief du projet"><br>
 
-            <label>Informations Contact Client :</label><br>
-            <input type="text" name="client_surname" class="form-control" placeholder="Prénom du client"><br>
-            <input type="text" name="client_lastname" class="form-control" placeholder="Nom du client"><br>
-            <input type="text" name="client_phone" class="form-control" placeholder="Téléphone du client"><br>
-            <input type="text" name="client_mail" class="form-control" placeholder="Mail du client"><br>
-            <input type="text" name="client_city" class="form-control" placeholder="Ville du client"><br>
-            <input type="text" name="client_postal" class="form-control" placeholder="Code postal du client"><br>
-            <input type="text" name="client_adress" class="form-control" placeholder="Adresse du client"><br>
+            <label for="infsclient">Informations Contact Client :</label><br>
+            <input type="text" id="client_surname" class="form-control" placeholder="Prénom du client"><br>
+            <input type="text" id="client_lastname" class="form-control" placeholder="Nom du client"><br>
+            <input type="text" id="client_phone" class="form-control" placeholder="Téléphone du client"><br>
+            <input type="text" id="client_mail" class="form-control" placeholder="Mail du client"><br>
+            <input type="text" id="client_city" class="form-control" placeholder="Ville du client"><br>
+            <input type="text" id="client_postal" class="form-control" placeholder="Code postal du client"><br>
+            <input type="text" id="client_adress" class="form-control" placeholder="Adresse du client"><br>
 
-            <label>Informations Contact Décisionnaire :</label><br>
-            <input type="text" name="deci_surname" class="form-control" placeholder="Prénom du décisionnaire"><br>
-            <input type="text" name="deci_lastname" class="form-control" placeholder="Nom du décisionnaire"><br>
-            <input type="text" name="deci_phone" class="form-control" placeholder="Téléphone du décisionnaire"><br>
-            <input type="text" name="deci_mail" class="form-control" placeholder="Mail du décisionnaire"><br>
-            <input type="text" name="deci_city" class="form-control" placeholder="Ville du décisionnaire"><br>
-            <input type="text" name="deci_postal" class="form-control" placeholder="Code postal du décisionnaire"><br>
-            <input type="text" name="deci_adress" class="form-control" placeholder="Adresse du décisionnaire"><br>
+            <label for="infsdecis">Informations Contact Décisionnaire :</label><br>
+            <input type="text" id="deci_surname" class="form-control" placeholder="Prénom du décisionnaire"><br>
+            <input type="text" id="deci_lastname" class="form-control" placeholder="Nom du décisionnaire"><br>
+            <input type="text" id="deci_phone" class="form-control" placeholder="Téléphone du décisionnaire"><br>
+            <input type="text" id="deci_mail" class="form-control" placeholder="Mail du décisionnaire"><br>
+            <input type="text" id="deci_city" class="form-control" placeholder="Ville du décisionnaire"><br>
+            <input type="text" id="deci_postal" class="form-control" placeholder="Code postal du décisionnaire"><br>
+            <input type="text" id="deci_adress" class="form-control" placeholder="Adresse du décisionnaire"><br>
 
-            <label>Date de début du projet :</label><br>
-            <input type="text" name="brief" class="form-control" placeholder="Date de début"><br><br>
+            <label for="debproj">Date de début du projet :</label><br>
+            <input type="text" id="debproj" class="form-control" placeholder="Date de début"><br>
 
-            <label>Date de fin du projet :</label><br>
-            <input type="text" name="brief" class="form-control"placeholder="Date de fin"><br><br>
+            <label for="finproj">Date de fin du projet :</label><br>
+            <input type="text" id="finproj" class="form-control"placeholder="Date de fin"><br><br>
 
             <!-- UPLOAD GANTT -->
 
-            <input type="submit" class="btn btn-default" name="Suite">
+            <input type="submit" class="btn btn-success" value="Continuer">
 
         </form>
     </div>
