6336ef159fbb944b872b7fba2faac5ead2db7649
Oricy <d.duvernois@student.isartdigital.com>
2016-10-12 14:06:07 +0200

    display date (d-m-Y) of uploaded document

--- templates/_upload_document.php
+++ templates/_upload_document.php
@@ -1,10 +1,12 @@
 <div class="container">
     <div class="row">
-        <?php 
+        <?php
         foreach ($documentUploads as $upload) {
-            echo '<div class="col-xs-12">';
+            $date = date_create($upload['created_at']);
+            echo '<div class="col-xs-12 fileUploaded">';
             echo '<a type="button" class="btn btn-primary" href="files/'.$upload['path_to'].'">'.$upload['name'].'</a>';
             echo '<a href="deletedocument.php?id='.$upload['id'].'"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>';
+            echo '<span class="text-right">'.date_format($date, 'd-m-Y').'</span>';
             echo '</div>';
         } 
         ?>
